579a1b23904ce70475fafa57ec5925fa224597f3
Keshav Singh <45248239+keshav2995@users.noreply.github.com>
2020-12-14 16:51:08 +0900

    Update teland_plots.py

--- teland_plots.py
+++ teland_plots.py
@@ -16,31 +16,35 @@ class Teland:
 
     gamma = 1.4   # value of adiabitc index of gas'))
 
-    def __init__(self, m, t, p, alpha, M1, var_plots=False):
-        if not var_plots:
-            self.m = m     # value of maximum camber as a fraction of chord(0-1)'))
-            self.t = t     # value of maximum thickness as a fraction of chord(0-1)'))
-            self.p = p      # the location of maximum thickness as a fraction of chord(0-1)'))
-            self.alpha = alpha     # the value of angle of attack'))
-            self.M1 = M1     # the value of free stream Mach Number'))
+    def __init__(self, m, t, p, alpha, M1):
+        self.m = m     # value of maximum camber as a fraction of chord(0-1)'))
+        self.t = t     # value of maximum thickness as a fraction of chord(0-1)'))
+        self.p = p      # the location of maximum thickness as a fraction of chord(0-1)'))
+        self.alpha = alpha     # the value of angle of attack'))
+        self.M1 = M1     # the value of free stream Mach Number'))
 
-        else:
-            self.M1 = np.linspace(int(M1[0]),int(M1[1]),10)
-            self.t = np.linspace(float(t[0]),float(t[1]),10)
-            self.p = np.linspace(float(p[0]),float(p[1]),10)
-            self.alpha = np.linspace(int(alpha[0]),int(alpha[1]),10)
-            self.m = np.linspace(float(m[0]),float(m[1]),10)
+        # else:
+        #     self.m = np.linspace(-0.01,0.01,25)
+        #     self.t = np.linspace(0.03,0.06,5)
+        #     self.p = np.linspace(0.1,0.5,5)
+        #     self.alpha = np.linspace(0,5,5)
+        #     self.M1 = np.linspace(1,4,5)
+            # self.m = np.linspace(float(m[0]),float(m[1]),10)
+            # self.t = np.linspace(float(t[0]),float(t[1]),10)
+            # self.p = np.linspace(float(p[0]),float(p[1]),10)
+            # self.alpha = np.linspace(int(alpha[0]),int(alpha[1]),10)
+            # self.M1 = np.linspace(int(M1[0]),int(M1[1]),10)
 
-    @classmethod
-    def from_user_input(self):
-        return self(
-            input('Enter range of M separated by comma: ').split(','), #1,4
-            input('Enter range of t separated by comma: ').split(','), #0.03,0.06
-            input('Enter range of p separated by comma:: ').split(','), #0.1,0.5
-            input('Enter range of alpha separated by comma:: ').split(','), #0,5
-            input('Enter range of m separated by comma:: ').split(','), #-0.01,0.01
-            input('Enter "True" for plots:')
-        )
+    # @classmethod
+    # def from_user_input(self):
+    #     return self(
+    #         input('Enter range of m separated by comma:: ').split(','), #-0.01,0.01
+    #         input('Enter range of t separated by comma: ').split(','), #0.03,0.06
+    #         input('Enter range of p separated by comma:: ').split(','), #0.1,0.5
+    #         input('Enter range of alpha separated by comma:: ').split(','), #0,5
+    #         input('Enter range of M separated by comma: ').split(','), #1,4
+    #         input('Enter "True" for plots:')
+    #     )
 
     def initial_variables(self):
         self.y_u = (self.t/2+self.m)
@@ -156,59 +160,246 @@ class Teland:
         self.Cd = self.cn*np.sin(self.alpha1)+self.ca*np.cos(self.alpha1)
         return self.Cl, self.Cd
 
+    @staticmethod
+    def visualization(df_t, df_p, df_alpha, df_M):
+        # Create traces
 
-    def visualization(self):
-        #-------*******todo*******--------#
-        for i in self.var_M:
-            cl_,cd_ = Teland.final_calc()
-        fig = make_subplots(rows=2, cols=2,
-                            specs=[[{"secondary_y": True}, {"secondary_y": True}],
-                                [{"secondary_y": True}, {"secondary_y": True}]])
+        fig = make_subplots(rows=4, cols=2,
+                    subplot_titles=("Cl/Cd vs m (for M) [SE theory]", 
+                    "Cl/Cd vs m (for M) [Ackeret theory]", 
+                    "Cl/Cd vs m (for t) [SE theory]", 
+                    "Cl/Cd vs m (for t) [Ackeret theory]",
+                    "Cl/Cd vs m (for p) [SE theory]",
+                    "Cl/Cd vs m (for p) [Ackeret theory]",
+                    "Cl/Cd vs m (for alpha) [SE theory]",
+                    "Cl/Cd vs m (for alpha) [Ackeret theory]"
+                    ),
+                    specs=[[{"secondary_y": True}, {"secondary_y": True}],
+                           [{"secondary_y": True}, {"secondary_y": True}],
+                           [{"secondary_y": True}, {"secondary_y": True}],
+                           [{"secondary_y": True}, {"secondary_y": True}]])
 
-        # 1st left
-        fig.add_trace(
-            go.Scatter(x=self.var_m, y=Teland.final_calc(), name="CL/CD vs m for var M"),
-            row=1, col=1, secondary_y=False,
-        )
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,1],
+                            mode='lines',
+                            name=df_M.columns[1]),
+                            row=1, col=1, secondary_y=False,)
 
-        fig.add_trace(
-            go.Scatter(x=df['Time'], y=df['BB'], name="BB"),
-            row=1, col=1, secondary_y=True,
-        )
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,3],
+                            mode='lines',
+                            name=df_M.columns[3]),
+                            row=1, col=1, secondary_y=False,)
 
-        # 1rd right
-        fig.add_trace(
-            go.Scatter(x=df['Time'], y=df['Price'], name="Price"),
-            row=1, col=2, secondary_y=False,
-        )
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,5],
+                            mode='lines', 
+                            name=df_M.columns[5]),
+                            row=1, col=1, secondary_y=False,)
 
-        fig.add_trace(
-            go.Scatter(x=df['Time'], y=df['MACD'], name="MACD"),
-            row=1, col=2, secondary_y=True,
-        )
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,7],
+                            mode='lines', 
+                            name=df_M.columns[7]),
+                            row=1, col=1, secondary_y=False,)
 
-        # Bottom left
-        fig.add_trace(
-            go.Scatter(x=df['Time'], y=df['Price'], name="Price"),
-            row=2, col=1, secondary_y=False,
-        )
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,9],
+                            mode='lines', 
+                            name=df_M.columns[9]),
+                            row=1, col=1, secondary_y=False,)                         
 
-        fig.add_trace(
-            go.Scatter(x=df['Time'], y=df['EMA'], name="EMA"),
-            row=2, col=1, secondary_y=True,
-        )
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,2],
+                            mode='lines',
+                            name=df_M.columns[2]),
+                            row=1, col=2, secondary_y=False,)
 
-        # Bottom right
-        fig.add_trace(
-            go.Scatter(x=df['Time'], y=df['Price'], name="Price"),
-            row=2, col=2, secondary_y=False,
-        )
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,4],
+                            mode='lines',
+                            name=df_M.columns[4]),
+                            row=1, col=2, secondary_y=False,)
 
-        fig.add_trace(
-            go.Scatter(x=df['Time'], y=df['MA'], name="MA"),
-            row=2, col=2, secondary_y=True,
-        )
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,6],
+                            mode='lines', 
+                            name=df_M.columns[6]),
+                            row=1, col=2, secondary_y=False,)
 
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,8],
+                            mode='lines', 
+                            name=df_M.columns[8]),
+                            row=1, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_M.iloc[:,0], y=df_M.iloc[:,10],
+                            mode='lines', 
+                            name=df_M.columns[10]),
+                            row=1, col=2, secondary_y=False,)                        
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,1],
+                            mode='lines',
+                            name=df_t.columns[1]),
+                            row=2, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,3],
+                            mode='lines',
+                            name=df_t.columns[3]),
+                            row=2, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,5],
+                            mode='lines', 
+                            name=df_t.columns[5]),
+                            row=2, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,7],
+                            mode='lines', 
+                            name=df_t.columns[7]),
+                            row=2, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,9],
+                            mode='lines', 
+                            name=df_t.columns[9]),
+                            row=2, col=1, secondary_y=False,)                        
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,2],
+                            mode='lines',
+                            name=df_t.columns[2]),
+                            row=2, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,4],
+                            mode='lines',
+                            name=df_t.columns[4]),
+                            row=2, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,6],
+                            mode='lines', 
+                            name=df_t.columns[6]),
+                            row=2, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,8],
+                            mode='lines', 
+                            name=df_t.columns[8]),
+                            row=2, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_t.iloc[:,0], y=df_t.iloc[:,10],
+                            mode='lines', 
+                            name=df_t.columns[10]),
+                            row=2, col=2, secondary_y=False,)                        
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,1],
+                            mode='lines',
+                            name=df_p.columns[1]),
+                            row=3, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,3],
+                            mode='lines',
+                            name=df_p.columns[3]),
+                            row=3, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,5],
+                            mode='lines', 
+                            name=df_p.columns[5]),
+                            row=3, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,7],
+                            mode='lines', 
+                            name=df_p.columns[7]),
+                            row=3, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,9],
+                            mode='lines', 
+                            name=df_p.columns[9]),
+                            row=3, col=1, secondary_y=False,)                        
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,2],
+                            mode='lines',
+                            name=df_p.columns[2]),
+                            row=3, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,4],
+                            mode='lines',
+                            name=df_p.columns[4]),
+                            row=3, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,6],
+                            mode='lines', 
+                            name=df_p.columns[6]),
+                            row=3, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,8],
+                            mode='lines', 
+                            name=df_p.columns[8]),
+                            row=3, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_p.iloc[:,0], y=df_p.iloc[:,10],
+                            mode='lines', 
+                            name=df_p.columns[10]),
+                            row=3, col=2, secondary_y=False,)                        
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,1],
+                            mode='lines',
+                            name=df_alpha.columns[1]),
+                            row=4, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,3],
+                            mode='lines',
+                            name=df_alpha.columns[3]),
+                            row=4, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,5],
+                            mode='lines', 
+                            name=df_alpha.columns[5]),
+                            row=4, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,7],
+                            mode='lines', 
+                            name=df_alpha.columns[7]),
+                            row=4, col=1, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,9],
+                            mode='lines', 
+                            name=df_alpha.columns[9]),
+                            row=4, col=1, secondary_y=False,)                         
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,2],
+                            mode='lines',
+                            name=df_alpha.columns[2]),
+                            row=4, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,4],
+                            mode='lines',
+                            name=df_alpha.columns[4]),
+                            row=4, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,6],
+                            mode='lines', 
+                            name=df_alpha.columns[6]),
+                            row=4, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,8],
+                            mode='lines', 
+                            name=df_alpha.columns[8]),
+                            row=4, col=2, secondary_y=False,)
+
+        fig.add_trace(go.Scatter(x=df_alpha.iloc[:,0], y=df_alpha.iloc[:,10],
+                            mode='lines', 
+                            name=df_alpha.columns[10]),
+                            row=4, col=2, secondary_y=False,)                        
+
+        # Update xaxis properties
+        fig.update_xaxes(title_text="Values of m", row=1, col=1)
+        fig.update_xaxes(title_text="Values of m", row=1, col=2)
+        fig.update_xaxes(title_text="Values of m", row=2, col=1)
+        fig.update_xaxes(title_text="Values of m", row=2, col=2)
+        fig.update_xaxes(title_text="Values of m", row=3, col=1)
+        fig.update_xaxes(title_text="Values of m", row=3, col=2)
+        fig.update_xaxes(title_text="Values of m", row=4, col=1)
+        fig.update_xaxes(title_text="Values of m", row=4, col=2)
+
+        # Update yaxis properties
+        fig.update_yaxes(title_text="Cl/Cd ratio", row=1, col=1)
+        fig.update_yaxes(title_text="Cl/Cd ratio", row=1, col=2)
+        fig.update_yaxes(title_text="Cl/Cd ratio", row=2, col=1)
+        fig.update_yaxes(title_text="Cl/Cd ratio", row=2, col=2)
+        fig.update_yaxes(title_text="Cl/Cd ratio", row=3, col=1)
+        fig.update_yaxes(title_text="Cl/Cd ratio", row=3, col=2)
+        fig.update_yaxes(title_text="Cl/Cd ratio", row=4, col=1)
+        fig.update_yaxes(title_text="Cl/Cd ratio", row=4, col=2)
+
+        fig.layout.update(height=1080, width=1280, showlegend=False, hovermode='closest')
 
         fig.show()
 
@@ -224,13 +415,60 @@ def main():
     C_lift_at, C_drag_at = t2.ackeret_solution()
     print("(i)Sectional lift coefficient by AT: {} \n (ii) Sectional wave drag coefficient by AT: {}".format(C_lift_at, C_drag_at))
 
-    #Visualization
-    # cl_ci = []
-    # for i in np.linspace(0.03,0.06,10):
-    #     C_lift, C_drag = Teland.final_calc()
-    #     ratio = C_lift/C_drag
-    #     cl_ci.append(ratio)
+    seq = [1,2,3,4]
+    names = ['t','p','alpha','M']
+    #for cl/cd vs m (given val)
+    def get_plot_data(*args):  #m, t, p, alpha, M1
+        df_x = pd.DataFrame()
+        arg = args
+        df_x['m value'] = arg[0]
+        global i_v
+        i_ = i_v
+        for x in arg[i_]:
+            ratio_cl_cd_set = []
+            ratio_cl_cd_at = []
+            for y in arg[0]: #---> m
+                if i_ ==1: 
+                    t3 = Teland(m = y, t = x, p = arg[2], alpha = arg[3], M1 = arg[4]) 
+                    t4 = Teland(y,x,arg[2],arg[3],arg[4])
+                elif i_==2:
+                    t3 = Teland(m = y, t = arg[1], p = x, alpha = arg[3], M1 = arg[4])
+                    t4 = Teland(y,arg[1],x,arg[3],arg[4])
+                elif i_==3:
+                    t3 = Teland(m = y, t = arg[1], p = arg[2], alpha = x, M1 = arg[4])
+                    t4 = Teland(y,arg[1],arg[2],x,arg[4])
+                else:
+                    t3 = Teland(m = y, t = arg[1], p = arg[2], alpha = arg[3], M1 = x)
+                    t4 = Teland(y,arg[1],arg[2],arg[3],x)
+                t3.runall()
+                C_l, C_d = t3.final_calc()
+                ratio = C_l[0]/C_d[0] 
+                ratio_cl_cd_set.append(ratio)
 
+                C_l_, C_d_ = t4.ackeret_solution()
+                ratio_ = C_l_/C_d_
+                ratio_cl_cd_at.append(ratio_)
+
+            df_x['Cl/Cd ratio for {}={} (set)'.format(names[i_-1],x)] = ratio_cl_cd_set
+            df_x['Cl/Cd ratio for {}={} (at)'.format(names[i_-1],x)] = ratio_cl_cd_at
+            
+        i_v += 1
+
+        return df_x
+
+    for i in seq:
+        if i==4:
+            df_M = get_plot_data(np.linspace(-0.01,0.01,25), 0.07, 0.5, 2, np.linspace(2,5,5))
+        elif i==1:
+            df_t = get_plot_data(np.linspace(-0.01,0.01,25), np.linspace(0.03,0.06,5), 0.5, 2, 3)
+        elif i==2:
+            df_p = get_plot_data(np.linspace(-0.01,0.01,25), 0.07, np.linspace(0.1,0.5,5), 2, 3)
+        elif i==3:
+            df_alpha = get_plot_data(np.linspace(-0.01,0.01,25), 0.07, 0.5, np.linspace(0,5,5), 3)
+
+
+    Teland.visualization(df_t, df_p, df_alpha, df_M)
 
 if __name__ == "__main__":
+    i_v = 1
     main()
